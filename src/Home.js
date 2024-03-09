9207afb83101218c6bc1b3716c28d318eee8ec64
bca202100688 <202100688@sigc.edu>
2024-03-09 04:18:46 -0800

    update

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="https://images.app.goo.gl/oZB7nXzhvop7E2vT6.png" alt="Welcome picture" />
+      <img className="home-img" src="https://cdn.shopify.com/s/files/1/0070/7032/files/ecommerce_20tools.png?v=1705700831" alt="Welcome picture" />
     </div>
   )
 }
