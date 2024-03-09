80cb4feba714eb2fbaa81f51bfd42f580799f624
bca202100688 <202100688@sigc.edu>
2024-03-09 03:47:49 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://images.app.goo.gl/oZB7nXzhvop7E2vT6.png" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-olive-ten.vercel.app"
